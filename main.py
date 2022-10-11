37b3f889fb992a327c501b4b550a4e8693c74965
Bpluy <36358690+Bpluy@users.noreply.github.com>
2022-10-11 09:56:38 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,49 +1,62 @@
 from flask import Flask, request, Response
 from viberbot import Api
 from viberbot.api.bot_configuration import BotConfiguration
-from viberbot.api.messages import VideoMessage
 from viberbot.api.messages.text_message import TextMessage
-import logging
-
 from viberbot.api.viber_requests import ViberConversationStartedRequest
 from viberbot.api.viber_requests import ViberFailedRequest
 from viberbot.api.viber_requests import ViberMessageRequest
 from viberbot.api.viber_requests import ViberSubscribedRequest
 from viberbot.api.viber_requests import ViberUnsubscribedRequest
 
+import time
+import logging
+import sched
+import threading
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
 app = Flask(__name__)
 viber = Api(BotConfiguration(
-    name='ITransBotTEST',
-    avatar='',
-    auth_token='4fe241c73de7dc0b-b7ec515681c531d7-65a29900ef6b1756'
+  name='PythonSampleBot',
+  avatar='http://viber.com/avatar.jpg',
+  auth_token='YOUR_AUTH_TOKEN_HERE'
 ))
-
 
 @app.route('/', methods=['POST'])
 def incoming():
-    logger.debug("received request. post data: {0}".format(request.get_data()))
-    # every viber message is signed, you can verify the signature using this method
-    if not viber.verify_signature(request.get_data(), request.headers.get('X-Viber-Content-Signature')):
-        return Response(status=403)
+	logger.debug("received request. post data: {0}".format(request.get_data()))
 
-    # this library supplies a simple way to receive a request object
-    viber_request = viber.parse_request(request.get_data())
+	viber_request = viber.parse_request(request.get_data().decode('utf8'))
 
-    if isinstance(viber_request, ViberMessageRequest):
-        message = viber_request.message
-        # lets echo back
-        viber.send_messages(viber_request.sender.id, [
-            message
-        ])
-    elif isinstance(viber_request, ViberSubscribedRequest):
-        viber.send_messages(viber_request.get_user.id, [
-            TextMessage(text="thanks for subscribing!")
-        ])
-    elif isinstance(viber_request, ViberFailedRequest):
-        logger.warn("client failed receiving message. failure: {0}".format(viber_request))
+	if isinstance(viber_request, ViberMessageRequest):
+		message = viber_request.message
+		viber.send_messages(viber_request.sender.id, [
+			message
+		])
+	elif isinstance(viber_request, ViberConversationStartedRequest) \
+			or isinstance(viber_request, ViberSubscribedRequest) \
+			or isinstance(viber_request, ViberUnsubscribedRequest):
+		viber.send_messages(viber_request.sender.id, [
+			TextMessage(None, None, viber_request.get_event_type())
+		])
+	elif isinstance(viber_request, ViberFailedRequest):
+		logger.warn("client failed receiving message. failure: {0}".format(viber_request))
 
-    return Response(status=200)
+	return Response(status=200)
+
+def set_webhook(viber):
+	viber.set_webhook('https://mybotwebserver.com:8443/')
 
 if __name__ == "__main__":
-    context = ('server.crt', 'server.key')
-    app.run(host='0.0.0.0', port=443, debug=True, ssl_context=context)
+	scheduler = sched.scheduler(time.time, time.sleep)
+	scheduler.enter(5, 1, set_webhook, (viber,))
+	t = threading.Thread(target=scheduler.run)
+	t.start()
+
+	context = ('server.crt', 'server.key')
+	app.run(host='0.0.0.0', port=8443, debug=True, ssl_context=context)
